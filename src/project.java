a91b119ac45999b28b6d2b4b6758510fbc816249
Laurie <laurie201@protonmail.ch>
2023-07-10 13:39:44 +0100

    Initial commit

---
+++ src/project.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class project {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // ????
+        System.out.println("type: ");
+        String message = scanner.nextLine();
+        System.out.println(message + message + message);
+    }
+}
