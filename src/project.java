4cd03bde0b857e357674dd7b76ac71c75c159b3e
Laurie <laurie201@protonmail.ch>
2023-07-10 18:21:50 +0100

    remove ???

--- src/project.java
+++ src/project.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class project {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // ????
+        Scanner scanner = new Scanner(System.in);
         System.out.println("type: ");
         String message = scanner.nextLine();
         System.out.println(message + message + message);
